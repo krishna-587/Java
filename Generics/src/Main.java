23555628a5a79d5f46e9e031adad01d8ea58775c
Singh <krishna-gopal.a.singh@capgemini.com>
2026-08-06 13:07:24 +0530

    Added generics note and main class

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+
 public class Main {
+    // why need of Generics
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int arr[] = new int[5];
+        // list is of type Object, can store any type of object(value)
+        ArrayList list = new ArrayList();
+
+        list.add("Krishna");  // String
+        list.add(10);  // Integer
+        list.add(10.5);  // Double
+
+        // as I've not defined the type of value a list can store, so it is storing all types of values
+        // Every class in java extends Object class, so list is storing all types of values as Object type
+        // If I want to get the value from the list, I need to typecast it to the original type of value, otherwise it will give an exception (ClassCastException)
+        String name = (String) list.get(0);
+        int number = (Integer) list.get(1);
+
+        // So now if I want to store any specific type of value in the list then I can define its type so that it can make arraylist of that type only
+
+        // This is because of Generics, Generics is a feature of Java that allows us to define the type of value a collection can store.
+        // It is a way to provide type safety to our code.
+
+        ArrayList<String> list1= new ArrayList<String>();
+        list1.add("Krishna");
+        list1.add("Hello");
+
+        String str1 = list1.get(1); // will return string value, no need of typecasting
+
     }
+
+
 }
--- Generics/src/Note.md
+++ Generics/src/Note.md
@@ -1,1 +1,10 @@
 # Generics
+Introduced in Java 5, generics allow you to create classes, interfaces, and methods with a placeholder for types. This enables type safety and eliminates the need for casting when working with collections.
+
+### Benefits of Generics
+1. **Type Safety**: Generics provide compile-time type checking, which helps catch errors early in the development process. This reduces the risk of `ClassCastException` at runtime.
+2. **Elimination of Casting**: With generics, you can avoid explicit casting when  working with collections. This makes the code cleaner and easier to read.
+3. **No compile time checking**: Generics allow you to create classes, interfaces, and methods that can operate on different types without the need for explicit casting. This makes your code more flexible and reusable.
+
+### Generic Type
+A generic type is a class or interface that is parameterized over types. For example, a generic class can work with any type specified by the user, and that type can be enforced at compile time.
